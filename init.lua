a7e176909c1b9406e9da7fdf0102f46fcfb1837a
JCSantos <jcs0969@icloud.com>
2025-07-07 09:21:45 +0200

    JC - Default Session + init.

--- init.lua
+++ init.lua
@@ -1,2 +1,7 @@
 vim.o.termguicolors = true
 require("config.lazy")
+local session = vim.fn.expand("~/.config/nvim/DefaultSession.vim")
+if vim.fn.filereadable(session) == 1 then
+  vim.cmd("source " .. session)
+end
+
